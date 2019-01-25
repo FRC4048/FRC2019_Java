d49cc3ef588f1a59c57c17368b4e2e795801a162
Ben-Krugman <benjamin.krugman@gmail.com>
2019-01-24 20:48:27 -0500

    Group Command for alignment DONE

--- src/main/java/org/usfirst/frc4048/commands/DriveAlignGroup.java
+++ src/main/java/org/usfirst/frc4048/commands/DriveAlignGroup.java
@@ -13,11 +13,11 @@ import org.usfirst.frc4048.utils.CameraDistance;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class LimelightAlignToTargetGroup extends CommandGroup {
+public class DriveAlignGroup extends CommandGroup {
   /**
    * Add your docs here.
    */
-  public LimelightAlignToTargetGroup() {
+  public DriveAlignGroup() {
     // Add Commands here:
     // e.g. addSequential(new Command1());
     // addSequential(new Command2());
@@ -34,33 +34,12 @@ public class LimelightAlignToTargetGroup extends CommandGroup {
     // e.g. if Command1 requires chassis, and Command2 requires arm,
     // a CommandGroup containing them would require both the chassis and the
     // arm.
-    final double maxSpeed = 0.5;
-    final double minSpeed = 0.35;
-    double forward;
-    double horizontal;
-    final double distanceAway = 20.0;
-    CameraDistance targetDistance = Robot.drivetrainSensors.getTargetDistance();
-    if(targetDistance == null) {
-      forward = 0;  
-      horizontal = 0;
-      System.out.println("----------------------No target detected----------------------");
-      return;
-    } else {
-      forward = targetDistance.getForward();
-      horizontal = targetDistance.getSideways();
-      SmartDashboard.putNumber("Forward Distance", forward);
-      SmartDashboard.putNumber("Horizontal Distnace", horizontal);
-    }
-
-    final double angle = Math.toDegrees(Math.atan(horizontal/(forward-distanceAway)));
-    final double moveDistance = Math.sqrt(Math.pow((forward-distanceAway), 2) + Math.pow(horizontal, 2));
-
-    addSequential(new RotateAngle(0));//step 1  THIS WILL CHANGE
-    SmartDashboard.putNumber("move Distance", moveDistance);
-    SmartDashboard.putNumber("angle 123", angle);
-    addSequential(new DriveDistanceMaintainAngle(moveDistance, angle, -maxSpeed, -minSpeed)); //step 2
-    // addSequential(new DriveTargetCenter(distanceAway, -maxSpeed)); //step 3
-    // addSequential(new LimelightOff());
+    addSequential(new LimelightOn());
+    addSequential(new RotateAngle(0)); //STEP 1
+    addSequential(new WaitCommand(0.3));
+    addSequential(new DriveAlignPhase2(0.2, 0.4, false)); //STEP 2
+    addSequential(new DriveAlignPhase3(0.25, false)); //STEP 3
+    addSequential(new LimelightOff());
   }
 }
  
--- src/main/java/org/usfirst/frc4048/commands/WaitCommand.java
+++ src/main/java/org/usfirst/frc4048/commands/WaitCommand.java
@@ -7,47 +7,31 @@
 
 package org.usfirst.frc4048.commands;
 
-import org.usfirst.frc4048.Robot;
-
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
 
-public class ScheduleLimelightAlign extends Command {
-  private double angle; 
-  private final double ANGLE_MARGIN_VALUE = 2;
-  private boolean done;
-  public ScheduleLimelightAlign() {
+public class WaitCommand extends Command {
+  private double seconds;
+  public WaitCommand(double seconds) {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
+    this.seconds = seconds;
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-    angle = 0;
-    done = false;
-    Scheduler.getInstance().add(new RotateAngle(angle));
-    setTimeout(2);
+    setTimeout(seconds);
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    // System.out.println("SCHEDULER ALIGNMENT COMMAND IS RUNNING");
-    // Scheduler.getInstance().add(new LimelightOn());
-    if(Math.abs(Robot.drivetrain.getGyro() - angle) <= ANGLE_MARGIN_VALUE){
-      Scheduler.getInstance().add(new LimelightAlignToTargetGroup());
-      done = true;
-    }
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    if(isTimedOut()) {
-      System.out.println("Timedout!!!!!!!!");
-    }
-    return done || isTimedOut();
+    return isTimedOut();
   }
 
   // Called once after isFinished returns true
@@ -59,5 +43,6 @@ public class ScheduleLimelightAlign extends Command {
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    end();
   }
 }
