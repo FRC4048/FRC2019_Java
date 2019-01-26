61f60a27791f7129048c57e1c4d006ca16236de6
Ben-Krugman <benjamin.krugman@gmail.com>
2019-01-26 16:24:12 -0500

    RobotMap setup

--- src/main/java/org/usfirst/frc4048/RobotMap.java
+++ src/main/java/org/usfirst/frc4048/RobotMap.java
@@ -26,6 +26,7 @@ public class RobotMap {
 
   public static final double SWERVE_DRIVE_ENCODER_DISTANCE_PER_TICK = 0.0942478739;
 
+  //CAN ID
   public static final int FRONT_LEFT_DRIVE_MOTOR_ID = 6; 
   public static final int FRONT_RIGHT_DRIVE_MOTOR_ID = 5; 
   public static final int REAR_LEFT_DRIVE_MOTOR_ID = 7;
@@ -35,16 +36,36 @@ public class RobotMap {
   public static final int REAR_LEFT_STEER_MOTOR_ID = 3; 
   public static final int REAR_RIGHT_STEER_MOTOR_ID = 4;
 
+  public static final int DRIVE_PIGEON_ID = 9;
+  public static final int PCM_CAN_ID = 10;  
+  public static final int ELEVATOR_MOTOR_ID = 11;
+  public static final int CARGO_INTAKE_MOTOR_ID = 12;
+
+
+  //ANALOG INPUTS
   public static final int SWERVE_DRIVE_ANALOG_INPUT_FRONT_RIGHT_ID = 0;
   public static final int SWERVE_DRIVE_ANALOG_INPUT_FRONT_LEFT_ID = 1;
   public static final int SWERVE_DRIVE_ANALOG_INPUT_REAR_RIGHT_ID = 3;
   public static final int SWERVE_DRIVE_ANALOG_INPUT_REAR_LEFT_ID = 2;
+  
+  public static final int CLIMBER_DISTANCE_SENSOR_LEFT_ID = 4;
+  public static final int CLIMBER_DISTANCE_SENSOR_RIGHT_ID = 5;
+  public static final int ALIGNMENT_DISTANCE_SENSOR_ID = 6;
 
+  //DIGITAL INPUTS
+  public static final int CARGO_LIMIT_SWITCH_LEFT_ID = 1;
+  public static final int CARGO_LIMIT_SWITCH_RIGHT_ID = 2;
+  public static final int[] ELEVATOR_QUAD_ENCODER_ID = {3,4};
   public static final int SWERVE_DRIVE_ENCODER_A_ID = 5; 
   public static final int SWERVE_DRIVE_ENCODER_B_ID = 6;
 
-  public static final int DRIVE_PIGEON_ID = 9;
+  //PWM
+  public static final int WINCH_MOTOR_ID = 1;
 
-  public static final int PCM_CAN_ID = 10;
-  
+  //PCM
+  public static final int CARGO_PISTON_ID = 1;
+  public static final int HATCH_PANEL_PISTON_ID = 2;
+  public static final int HATCH_PANEL_WRIST_PISTON_ID = 3;
+  public static final int INTAKES_WRIST_PISTON_ID = 4;
+  public static final int CLIMBER_PISTONS_ID = 5;
 }
