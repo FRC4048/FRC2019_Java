3eef3646fdb58f353fb7cdb1298fa8ef292138c3
batchen1 <bat-chen@MacBook-Pro.localdomain>
2019-01-31 07:08:27 -0500

    no need to log LogError

--- src/main/java/org/usfirst/frc4048/commands/LogError.java
+++ src/main/java/org/usfirst/frc4048/commands/LogError.java
@@ -4,46 +4,39 @@ import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc4048.Robot;
 import org.usfirst.frc4048.utils.Logging;
 
-public class LogError extends LoggedCommand {
+public class LogError extends Command {
 
     public LogError() {
-		super(String.format(" is running"));
     }
 
     // Called just before this Command runs the first time
 	@Override
-	protected void loggedInitialize() {
+	protected void initialize() {
 		Robot.logging.traceMessage(Logging.MessageLevel.INFORMATION, "~~~DriveTeam Breakpoint~~~");
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	@Override
-	protected void loggedExecute() {
+	protected void execute() {
        
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
-	protected boolean loggedIsFinished() {
+	protected boolean isFinished() {
 		return true;
 	}
 
 	// Called once after isFinished returns true
 	@Override
-	protected void loggedEnd() {
+	protected void end() {
 
 	}
 
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	@Override
-	protected void loggedInterrupted() {
-        loggedEnd();
+	protected void interrupted() {
+        end();
 	}
-
-	@Override
-	protected void loggedCancel() {
-        loggedEnd();
-	}
-
 }
