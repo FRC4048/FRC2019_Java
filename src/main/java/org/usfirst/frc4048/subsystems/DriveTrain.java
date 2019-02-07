1251a45fbdd065657222e52c2bc9dc8a38488fab
ohad <ohadze@yahoo.com>
2019-02-06 22:16:08 -0500

    Merge branch 'bz-shuffleboard' of C:\Users\ohad\Documents\FRC2019\FRC2019.java with conflicts.

--- src/main/java/org/usfirst/frc4048/subsystems/DriveTrain.java
+++ src/main/java/org/usfirst/frc4048/subsystems/DriveTrain.java
@@ -105,10 +105,14 @@ public class DriveTrain extends Subsystem {
     driveRL.setNeutralMode(NeutralMode.Brake);
     driveRR.setNeutralMode(NeutralMode.Brake);
 
-    // driveFL.setSafetyEnabled(true);
-    // driveFR.setSafetyEnabled(true);
-    // driveRL.setSafetyEnabled(true);
-    // driveRL.setSafetyEnabled(true);
+    driveFL.setSafetyEnabled(true);
+    driveFL.setExpiration(0.5);
+    driveFR.setSafetyEnabled(true);
+    driveFR.setExpiration(0.5);
+    driveRL.setSafetyEnabled(true);
+    driveRL.setExpiration(0.5);
+    driveRR.setSafetyEnabled(true);
+    driveRR.setExpiration(0.5);
 
     analogInputFrontLeft = new AnalogInput(RobotMap.SWERVE_DRIVE_ANALOG_INPUT_FRONT_LEFT_ID);
     analogInputFrontRight = new AnalogInput(RobotMap.SWERVE_DRIVE_ANALOG_INPUT_FRONT_RIGHT_ID);
