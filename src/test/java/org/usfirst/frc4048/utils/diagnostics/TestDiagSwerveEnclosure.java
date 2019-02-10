be09740aaea400bf30141c565408c4739a37eb18
ohad <ohadze@yahoo.com>
2019-02-10 16:45:12 -0500

    Added last two diagnostics and tests

---
+++ src/test/java/org/usfirst/frc4048/utils/diagnostics/TestDiagSwerveEnclosure.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc4048.utils.diagnostics;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.usfirst.frc4048.swerve.drive.CanTalonSwerveEnclosure;
+
+import static org.mockito.Mockito.when;
+
+public class TestDiagSwerveEnclosure {
+
+    @Test
+    public void testSwerveInitially() throws Exception {
+        CanTalonSwerveEnclosure mockEnclosure = Mockito.mock(CanTalonSwerveEnclosure.class);
+        when(mockEnclosure.getEncPosition()).thenReturn(1);
+
+        DiagSwerveEnclosure classUnderTest = new DiagSwerveEnclosure("enclosure", 100, mockEnclosure, true);
+
+        when(mockEnclosure.getEncPosition()).thenReturn(51);
+        Assert.assertFalse(classUnderTest.getDiagResult());
+
+        when(mockEnclosure.getEncPosition()).thenReturn(-50);
+        Assert.assertFalse(classUnderTest.getDiagResult());
+
+        when(mockEnclosure.getEncPosition()).thenReturn(101);
+        Assert.assertTrue(classUnderTest.getDiagResult());
+
+        when(mockEnclosure.getEncPosition()).thenReturn(50);
+        Assert.assertTrue(classUnderTest.getDiagResult());
+    }
+
+    @Test
+    public void testSwerveAfterReset() throws Exception {
+        CanTalonSwerveEnclosure mockEnclosure = Mockito.mock(CanTalonSwerveEnclosure.class);
+        when(mockEnclosure.getEncPosition()).thenReturn(1);
+
+        DiagSwerveEnclosure classUnderTest = new DiagSwerveEnclosure("enclosure", 100, mockEnclosure, true);
+
+        when(mockEnclosure.getEncPosition()).thenReturn(101);
+        Assert.assertTrue(classUnderTest.getDiagResult());
+
+        classUnderTest.reset();
+        Assert.assertFalse(classUnderTest.getDiagResult());
+    }
+}
